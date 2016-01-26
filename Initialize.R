c1c0ab34ce7cfdc274e0039cf7d3a9ebfa5f9d39
Charl Marais <charl.marais@zestlife.co.za>
2016-01-26 13:05:51 +0200

    Project Start

---
+++ Initialize.R
@@ -0,0 +1,190 @@
+# Set Seed
+set.seed(1)
+
+# Disable Scientific Notation
+options(scipen = 999)
+
+#########################################################################################################
+# Load Libraries and Custom Functions #
+#######################################
+
+library(openxlsx)
+library(dplyr)
+library(ChainLadder)
+library(ggplot2)
+library(reshape)
+library(gbm)
+library(randomForest)
+library(AUC)
+library(xgboost)
+library(Ckmeans.1d.dp)
+library(data.table)
+library(dplyr)
+
+########################################################################################################################################
+########################################################################################################################################
+
+dbDisconnectAll <- function(){
+  ile <- length(dbListConnections(MySQL())  )
+  lapply( dbListConnections(MySQL()), function(x) dbDisconnect(x) )
+  cat(sprintf("%s connection(s) closed.\n", ile))
+}
+
+########################################################################################################################################
+########################################################################################################################################
+
+DateConv <- function(Cont){
+  
+  #############################################################################################
+  
+  Cont <- as.character(Cont)
+  Cont <- gsub(" ", "", Cont)
+  Cont <- gsub("/", "-", Cont)
+  Cont <- gsub("\\", "-", Cont, fixed = TRUE)
+  Cont <- gsub(".", "-", Cont, fixed = TRUE)
+  
+  #############################################################################################
+  
+  countr <- length(Cont)
+  
+  ContDf <- data.frame(Content = character(countr), 
+                       Six_Char = character(countr), 
+                       Leftyear = character(countr), 
+                       RightYear = character(countr), 
+                       YearFromNum = character(countr),
+                       Out = as.Date(countr, origin = "1899-12-30"),
+                       stringsAsFactors=FALSE)
+  ContDf$Content <- Cont 
+  ContDf$Content[ContDf$Content == ""] <- NA
+  ContDf$Out[is.na(ContDf$Content)] <- NA
+  
+  #############################################################################################
+  
+  ContDf$Six_Char[nchar(ContDf$Content) == 6] <- ContDf$Content[nchar(ContDf$Content) == 6]
+  
+  year <- strtrim(ContDf$Six_Char,2)
+  cent <- ifelse(as.numeric(year) < as.numeric(substr(format(Sys.Date(), "%Y"),3,4)),as.numeric(substr(format(Sys.Date(), "%Y"),1,2)),as.numeric(substr(format(Sys.Date(), "%Y"),1,2))-1)
+  cent[is.na(cent)] <- ""
+  month <- substr(ContDf$Six_Char,3,4)
+  day <- substr(ContDf$Six_Char,5,6)
+  Result6 <- paste(cent, year, "-", month, "-", day, sep = "")
+  Result6[Result6 == "--"] <- NA
+  ContDf$Six_Char <- Result6
+  ContDf$Content[!is.na(Result6)] <- NA
+  ContDf$Out[!is.na(ContDf$Six_Char)] <- ContDf$Six_Char[!is.na(ContDf$Six_Char)]
+  
+  #############################################################################################
+  
+  ContDf$YearFromNum[!is.na(as.numeric(ContDf$Content))] <- ContDf$Content[!is.na(as.numeric(ContDf$Content))]
+  ContDf$YearFromNum <- as.Date(as.numeric(ContDf$YearFromNum), origin = "1899-12-30")
+  ContDf$Content[!is.na(ContDf$YearFromNum)] <- NA
+  ContDf$Out[!is.na(ContDf$YearFromNum)] <- ContDf$YearFromNum[!is.na(ContDf$YearFromNum)]
+  
+  #############################################################################################
+  
+  ContDf$Leftyear[!is.na(as.numeric(substr(ContDf$Content,1,4)))] <- ContDf$Content[!is.na(as.numeric(substr(ContDf$Content,1,4)))]
+  LYDat <- ContDf$Leftyear
+  
+  leftprt <- as.numeric(substr(LYDat, 6, 7))
+  rightprt <- as.numeric(substr(LYDat, 9, 10))
+  
+  leftprt_ch <- ifelse(leftprt > 12, 1, 0)
+  leftprt_ch[is.na(leftprt_ch)] <- FALSE
+  
+  LYDat[leftprt_ch == 1] <- paste(substr(LYDat, 1, 4), "-", 
+                                  substr(LYDat, 9, 10), "-", 
+                                  substr(LYDat, 6, 7), sep = "")[leftprt_ch == 1]
+  
+  LYDat[LYDat == ""] <- NA
+  
+  ContDf$Leftyear <- LYDat
+  
+  ContDf$Content[!is.na(ContDf$Leftyear)] <- NA
+  ContDf$Out[!is.na(ContDf$Leftyear)] <- ContDf$Leftyear[!is.na(ContDf$Leftyear)]
+  
+  
+  #############################################################################################
+  
+  ContDf$RightYear[!is.na(ContDf$Content)] <- ContDf$Content[!is.na(ContDf$Content)]
+  RYDat <- ContDf$RightYear
+  
+  leftprt <- as.numeric(substr(RYDat, 1, 2))
+  rightprt <- as.numeric(substr(RYDat, 4, 5))
+  
+  leftprt_ch <- ifelse(leftprt > 12, 2, 1)
+  leftprt_ch[is.na(leftprt_ch)] <- 0
+  
+  RYDat[leftprt_ch == 2] <- paste(substr(RYDat, 7, 10), "-", 
+                                  substr(RYDat, 4, 5), "-", 
+                                  substr(RYDat, 1, 2), sep = "")[leftprt_ch == 2]
+  
+  RYDat[leftprt_ch == 1] <- paste(substr(RYDat, 7, 10), "-", 
+                                  substr(RYDat, 1, 2), "-", 
+                                  substr(RYDat, 4, 5), sep = "")[leftprt_ch == 1]
+  RYDat[RYDat == ""] <- NA
+  ContDf$RightYear <- RYDat
+  
+  ContDf$Content[!is.na(ContDf$RightYear)] <- NA
+  ContDf$Out[!is.na(ContDf$RightYear)] <- ContDf$RightYear[!is.na(ContDf$RightYear)]
+  
+  return(ContDf$Out)
+  
+}
+
+########################################################################################################################################
+########################################################################################################################################
+
+# Multiple plot function
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  library(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
---
+++ Lapse_Analysis.R
@@ -0,0 +1,158 @@
+#################################################################################################################################
+################################################### AssertLife Lapse Analysis ###################################################
+#################################################################################################################################
+
+#####################################################################################################
+# Setup #
+#########
+
+Path <- getwd()
+source(paste(Path, "/Initialize.R", sep = ""))
+
+#####################################################################################################
+# Load Data #
+#############
+
+# Load Correct Claims File Here
+Dat <- read.xlsx(paste(Path,"/Lapse_Data.xlsx", sep = ""), 
+                 sheet = "Lapse_Data", 
+                 detectDates = TRUE, 
+                 colNames = TRUE)
+
+Dat <- data.table(Dat)
+
+Increase <- Dat$Increase
+Increase[Increase == "NTU"] <- NA
+Increase <- as.numeric(Increase)
+
+Dat$Increase <- Increase
+
+colnames(Dat)[which(colnames(Dat) %in% "Smoker/Non-Smoker")] <- "Smoker_Non-Smoker"
+colnames(Dat)[which(colnames(Dat) %in% "Adjusted_x000D_.Commencement_x000D_.date")] <- "Comm_Dt"
+colnames(Dat)[which(colnames(Dat) %in% "Adjusted_x000D_.Voice_x000D_.logged_x000D_.date")] <- "Voice_Dt"
+colnames(Dat)[which(colnames(Dat) %in% "Adjusted_x000D_.status_x000D_.effective_x000D_.end.date")] <- "End_Dt"
+colnames(Dat)[which(colnames(Dat) %in% "Adjusted_x000D_.Status")] <- "Stat"
+colnames(Dat)[which(colnames(Dat) %in% "Active.quoted.Premium")] <- "Act_Prem"
+colnames(Dat)[which(colnames(Dat) %in% "Total.Amount.paid.since.inception.to.current.month")] <- "Tot_Pd"
+colnames(Dat)[which(colnames(Dat) %in% "Credit.Protection.-.Death.sum.assured")] <- "Death_Sum"
+colnames(Dat)[which(colnames(Dat) %in% "Credit.Protection.-.Disability.sum.assured")] <- "Disb_Sum"
+colnames(Dat)[which(colnames(Dat) %in% "Credit.Protection.-.Temporary.Disability.monthly.benefit")] <- "TTD_Sum"
+colnames(Dat)[which(colnames(Dat) %in% "Critical.Illness.Cover")] <- "CI_Sum"
+colnames(Dat)[which(colnames(Dat) %in% "Retrenchment.benefit")] <- "Retr_Sum"
+colnames(Dat)[which(colnames(Dat) %in% "RAF.benefit")] <- "RAF_Sum"
+
+str(Dat)
+head(Dat)
+
+Dat$Final_Stat <- 0
+Dat$Final_Stat[Dat$Stat == "NTU"] <- 1
+
+Dat_Save <- Dat
+
+full.features <- c("Gender", "Age", "Level.of.Income", "Education", "Postal.code", "Product.code", "Agent.name", "Act_Prem",
+                   "Death_Sum", "Disb_Sum", "TTD_Sum", "CI_Sum", "Retr_Sum", "RAF_Sum", "Comm_Dt", "Voice_Dt", "Duration",
+                   "Final_Stat", "Stat")
+
+Dat <- as.data.frame(Dat_Save)[,full.features]
+Dat <- Dat[rowSums(is.na(Dat)) < 13, ]
+
+# Dat$Final_Stat <- 0
+# Dat$Final_Stat[Dat$Stat == "LAP" & Dat$Duration == 5] <- 1
+
+Dat$Voice_Day <- as.numeric(format(Dat$Voice_Dt, "%d"))
+Dat$Voice_Month <- as.numeric(format(Dat$Voice_Dt, "%m"))
+
+Dat$Com_Day <- as.numeric(format(Dat$Comm_Dt, "%d"))
+Dat$Com_Month <- as.numeric(format(Dat$Comm_Dt, "%m"))
+
+Dat$rand <- runif(nrow(Dat))
+
+feature.names <- c("Gender", "Age", "Level.of.Income", "Education", "Postal.code", "Product.code", "Agent.name", "Act_Prem",
+                   "Death_Sum", "Disb_Sum", "TTD_Sum", "CI_Sum", "Retr_Sum", "RAF_Sum", "Voice_Day", "Voice_Month",
+                   "Com_Day", "Com_Month")
+
+train <- subset(Dat, rand < 0.8) 
+test <- subset(Dat, rand < 0.8)
+
+# put IDs for text variables - replace missings by 0 - boosting runs faster
+for (f in feature.names) {
+  if (class(train[[f]])=="character") {
+    levels <- unique(c(train[[f]], test[[f]])) # Combines all possible factors (both from train and test)
+    train[[f]] <- ifelse(is.na(train[[f]]), 0, as.integer(factor(train[[f]], levels=levels))) # Create a numeric ID
+    test[[f]]  <- ifelse(is.na(test[[f]]), 0, as.integer(factor(test[[f]],  levels=levels)))  # instead of a character ID (If NA then 0)
+  }
+  else if (class(train[[f]]) == "integer" | class(train[[f]]) == "numeric"){
+    train[[f]] <- ifelse(is.na(train[[f]]), 0, train[[f]]) # If NA then
+    test[[f]] <- ifelse(is.na(test[[f]]), 0, test[[f]])    # 0
+  }
+}
+
+tra <- train[,feature.names]
+
+h <- base::sample(nrow(train), 0.2 * nrow(train)) # Takes a sample of 20% of the training data
+
+# Split training data into 2 datasets
+dval <- xgb.DMatrix(data = data.matrix(tra[h,]), 
+                    label = train$Final_Stat[h])
+dtrain <- xgb.DMatrix(data = data.matrix(tra[-h,]), 
+                      label = train$Final_Stat[-h])
+
+watchlist <- list(val = dval, train = dtrain)
+
+param <- list(  objective           = "binary:logistic", 
+                booster             = "gbtree",
+                eta                 = 0.1,
+                max_depth           = 8,
+                subsample           = 0.7,
+                colsample_bytree    = 0.7
+)
+
+mod <- xgb.train(   params              = param, 
+                    data                = dtrain, 
+                    nrounds             = 1000,
+                    verbose             = 1, # Whether to print eoutput or not
+                    early.stop.round    = 30,
+                    watchlist           = watchlist,
+                    maximize            = TRUE,
+                    eval.metric         = "auc"        
+)
+
+cv.res <- xgb.cv(   params              = param, 
+                    data                = dtrain, 
+                    nrounds             = 100,
+                    verbose             = 1, # Whether to print output or not
+                    early.stop.round    = 30,
+                    watchlist           = watchlist,
+                    maximize            = TRUE,
+                    eval.metric         = "auc",
+                    nfold               = 4
+)
+
+test$pred <- predict(mod, data.matrix(test[, feature.names]))
+
+auc(roc(test$pred, as.factor(test$Final_Stat))) # 0.816134
+
+importance_matrix <- xgb.importance(feature.names, model = mod) # check if this goes faster via creation of dump file xgb.dump
+xgb.plot.importance(importance_matrix)
+xgb.plot.tree(feature_names = feature.names, model = mod, n_first_tree = 1)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
